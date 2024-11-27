1c79328092439a747be598a54e2e6a66d7ab3e5f
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-11-26 23:30:00 -0300

    feat: dashboard skeleton

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
-export default function Page() {
+import { Suspense } from 'react'
+
+import Loading from './loading'
+
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-4">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <Suspense fallback={<Loading />}>
+        <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+        </div>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </Suspense>
     </div>
   )
 }

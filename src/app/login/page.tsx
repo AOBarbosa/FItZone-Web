9f9497b6bce7668f2cb950799087c8806be3f599
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-11-27 02:14:37 -0300

    fix: fix merge error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,46 +1,54 @@
-import Image from 'next/image';
-import loginImage from '/public/login-image.png';
+import Image from 'next/image'
+
+import loginImage from '@/../public/login-image.png'
 
 export default function Login() {
   return (
     <div className="flex h-screen">
-      <div className="w-1/2 relative">
-        <Image 
-          src={loginImage} 
-          alt="Login Image" 
-          layout="fill" 
-          //objectFit="cover" 
-          className="object-cover w-full h-full"
+      <div className="relative w-1/2">
+        <Image
+          src={loginImage}
+          alt="Login Image"
+          layout="fill"
+          className="h-full w-full object-cover"
         />
       </div>
 
-      <div className="w-1/2 flex items-center justify-center bg-white">
+      <div className="flex w-1/2 items-center justify-center bg-white">
         <div className="w-full max-w-md px-8">
-          <h1 className="text-4xl font-bold text-center mb-8 text-black">FitZone</h1>
+          <h1 className="mb-8 text-center text-4xl font-bold text-black">
+            FitZone
+          </h1>
 
           <form>
             <div className="mb-6">
-              <label htmlFor="registerID" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="registerID"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Número da Matrícula
               </label>
               <input
                 type="text"
                 id="registerID"
                 name="registerID"
-                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-black shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 placeholder="Insira sua matrícula"
               />
             </div>
 
             <div className="mb-6">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Senha
               </label>
               <input
                 type="password"
                 id="password"
                 name="password"
-                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-black shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 placeholder="Insira sua senha"
               />
             </div>
@@ -48,7 +56,7 @@ export default function Login() {
             <div>
               <button
                 type="submit"
-                className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-black bg-[#D1F561] hover:bg-[#a7c44d] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#D1F561]"
+                className="w-full rounded-md border border-transparent bg-[#D1F561] px-4 py-2 text-black shadow-sm hover:bg-[#a7c44d] focus:outline-none focus:ring-2 focus:ring-[#D1F561] focus:ring-offset-2"
               >
                 Entrar
               </button>
@@ -57,5 +65,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-  );
-
+  )
+}

36bdbc0f0971373efc74ca0265bfda29cf6db309
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-11-26 23:16:04 -0300

    feat: sidebar dont appear in login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
-import { AppSidebar } from '@/components/sidebar/app-sidebar'
+import SideBar from '@/components/sidebar'
 import { ThemeProvider } from '@/components/theme-provider'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
-import { Separator } from '@/components/ui/separator'
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar'
 import { Toaster } from '@/components/ui/toaster'
 
 const geistSans = localFont({
@@ -53,32 +39,57 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-                <div className="flex items-center gap-2 px-4">
-                  <SidebarTrigger className="-ml-1" />
-                  <Separator orientation="vertical" className="mr-2 h-4" />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className="hidden md:block" />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Geral</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
-                </div>
-              </header>
-              {children}
-            </SidebarInset>
-          </SidebarProvider>
+          <SideBar>{children}</SideBar>
           <Toaster />
         </ThemeProvider>
       </body>
     </html>
   )
 }
+
+// // Componente marcado com "use client"
+// function ClientComponent({ children }: { children: React.ReactNode }) {
+//   'use client'
+
+//   const [isLoginPage, setIsLoginPage] = useState(false)
+
+//   useEffect(() => {
+//     if (typeof window !== 'undefined') {
+//       setIsLoginPage(window.location.href.includes('login'))
+//     }
+//   }, [])
+
+//   return (
+//     <>
+//       {isLoginPage ? (
+//         <>{children}</>
+//       ) : (
+//         <>
+//           <SidebarProvider>
+//             <AppSidebar />
+//             <SidebarInset>
+//               <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+//                 <div className="flex items-center gap-2 px-4">
+//                   <SidebarTrigger className="-ml-1" />
+//                   <Separator orientation="vertical" className="mr-2 h-4" />
+//                   <Breadcrumb>
+//                     <BreadcrumbList>
+//                       <BreadcrumbItem className="hidden md:block">
+//                         <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+//                       </BreadcrumbItem>
+//                       <BreadcrumbSeparator className="hidden md:block" />
+//                       <BreadcrumbItem>
+//                         <BreadcrumbPage>Geral</BreadcrumbPage>
+//                       </BreadcrumbItem>
+//                     </BreadcrumbList>
+//                   </Breadcrumb>
+//                 </div>
+//               </header>
+//               {children}
+//             </SidebarInset>
+//           </SidebarProvider>
+//         </>
+//       )}
+//     </>
+//   )
+// }
